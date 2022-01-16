1840eeb42f8d25b0c5aa4d35136302a8306bae44
lalmquist <43098342+lalmquist@users.noreply.github.com>
2022-01-16 13:02:18 -0800

    exercise5

--- section2/exercise5.cpp
+++ section2/exercise5.cpp
@@ -5,12 +5,24 @@
 #include<cmath>
 using namespace std;
 
+bool compare(pair<int,int> v1, pair<int,int> v2){
+    return v1.second < v2.second;
+}
+
 int countActivites(vector<pair<int,int> > activities){
     //Complete this method
+    int ans = 1;
+    int j = 0;
+    sort(activities.begin(),activities.end(),compare);
+
+    for(int i=1;i<activities.size();i++){
+        if (activities[i].first >= activities[j].second) {
+            ans += 1;
+            j = i;
+        }
+    }
     
-    
-    
-    
+    return ans;
 }
 
 
